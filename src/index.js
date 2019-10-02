340220371abc23d511eaebfe540b189989728392
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-02 10:57:49 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchImages();
+});
+
+function displayImages(json){
+  console.log(json);
+}
+function fetchImages(){
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  let returnString = "";
+  fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => displayImages(json));
+}
