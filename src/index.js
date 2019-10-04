707cca3443f4a3bd13d1515acdfc7e7e320f87f1
Andrew Davisson <adavisson@Andrews-MacBook-Air.local>
2019-10-04 10:28:51 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ function displayImages(json) {
 
 function fetchImages() {
   const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-  let returnString = "";
   fetch(imgUrl)
     .then(resp => resp.json())
     .then(json => {displayImages(json);
