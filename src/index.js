a6743c879135f8160a05531982a022328d66599c
Tsunamins <reillyamr@gmail.com>
2019-10-09 13:57:47 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -35,14 +35,3 @@ function fetchImages() {
 
 
 
-  
-//   function renderBooks(json) {
-//     const main = document.querySelector('main')
-//     json.forEach(book => {
-//       const h2 = document.createElement('h2')
-//       h2.innerHTML = `<h2>${book.name}</h2>`
-//       main.appendChild(h2)
-//     })
-//   }
-  
- 
