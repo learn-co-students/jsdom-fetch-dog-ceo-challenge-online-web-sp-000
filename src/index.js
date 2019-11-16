1d96bdc866c363105bdf84e5cec18579e2025860
Adil Naimi <adilnm23@hotmail.com>
2019-11-16 05:33:14 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+  chalengeOne();
+});
+
+let arr=[];
+  function chalengeOne() {
+    fetch('https://dog.ceo/api/breeds/image/random/4')
+      .then(function(response){
+        return response.json();
+      })
+
+      .then(function(json){
+        for(const elmt of json.message)
+           insertImage(elmt);
+
+      })
+  }
+  function insertImage(url) {
+      let image=document.createElement('img');
+      document.querySelector('#dog-image-container').appendChild(image);
+      image.src=url;
+  }
