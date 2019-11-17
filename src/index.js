fc6936eb2f4800f4b0fa3c85d146f4f6601c23b6
Adil Naimi <adilnm23@hotmail.com>
2019-11-17 01:54:04 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
-  chalengeOne();
+  challengeOne();
+  challengeTwo();
+  challengeFour()
 });
 
-let arr=[];
-  function chalengeOne() {
+  function challengeOne() {
     fetch('https://dog.ceo/api/breeds/image/random/4')
       .then(function(response){
         return response.json();
@@ -19,4 +20,40 @@ let arr=[];
       let image=document.createElement('img');
       document.querySelector('#dog-image-container').appendChild(image);
       image.src=url;
+  }
+
+  function challengeTwo() {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(function(response){
+        return response.json();
+      })
+
+      .then(function(json){
+        for(const key in json.message)
+           loadDogs(key);
+
+      })
+  }
+
+  function loadDogs(breed) {
+    let dog=document.createElement('li');
+    document.querySelector('#dog-breeds').appendChild(dog)
+    dog.innerText=breed;
+    dog.addEventListener('click', function (event) {
+      this.style.color='red'
+    })
+  }
+
+  function challengeFour() {
+    let options=document.querySelector('#breed-dropdown')
+    options.addEventListener('change', function (event) {
+      let li=document.querySelectorAll('li')
+        for(const i of li){
+          i.style.display = "block"
+          if (i.innerText.charAt(0)!=options.value) {
+            i.style.display = "none"
+          }
+        }
+
+    })
   }
