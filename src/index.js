d526df4f23a73039f29b9ff226cd3a891df5c6b8
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-11-26 14:29:28 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const dogsContainer = document.getElementById("dog-image-container")
+
+function addDog(dog) {
+    const tr = document.createElement('tr')
+
+    tr.innerHTML = `<img src = ${dog.message} >`
+
+    dogsContainer.append(tr)
+}
+
+function addDogs(dogs) {
+    dogs.message.forEach((dog) => addDog(dog))
+}
+
+function getDogs() {
+    fetch(imgUrl)
+        .then((response) => response.json())
+}
+
+getDogs()
+.then((dogs) => addDogs(dogs))
