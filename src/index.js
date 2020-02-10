af110b9d6b73625ee6675b76aad27ecf060a70a1
tomciv9000 <eugene.white@gmail.com>
2020-02-10 00:04:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+let breeds = [];
+
 document.addEventListener('DOMContentLoaded', function () {
     loadImages();
+    loadBreedOptions();
   });
 
 function loadImages() {
@@ -17,3 +20,53 @@ function addImage(dogPicUrl) {
   newImageElement.src = dogPicUrl;
   container.appendChild(newImageElement);
 }
+
+function loadBreedOptions() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+      .then(res => res.json())
+      .then(results => {
+  
+        breeds = Object.keys(results.message);
+        updateBreedList(breeds);
+        addBreedSelectListener();
+      });
+  }
+  
+  function updateBreedList(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+  }
+  
+  function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+  
+  function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+  }
+  
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
+  
+  function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+  
+  function updateColor(event) {
+    event.target.style.color = 'palevioletred';
+  }
