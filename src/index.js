9604380b35d0b7678af9d9be9a872ed7c83957df
tomciv9000 <eugene.white@gmail.com>
2020-02-09 23:49:05 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+  });
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+      .then(resp => resp.json())
+      .then(results => {
+        results.message.forEach(image => addImage(image))
+      });
+  }
+
+function addImage(dogPicUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let newImageElement = document.createElement('img');
+  newImageElement.src = dogPicUrl;
+  container.appendChild(newImageElement);
+}
