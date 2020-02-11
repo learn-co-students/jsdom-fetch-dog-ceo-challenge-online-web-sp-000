9c26bf224bd896528f8eef23d210ef80f75a530d
Paul Rosero <paul_rosero@yahoo.com>
2020-02-10 19:57:13 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+function fetchImg() {
+    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => console.log(fetchImg(json))   
+}
+
+function renderImg(json){
+    document.
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchImg()
+  })
