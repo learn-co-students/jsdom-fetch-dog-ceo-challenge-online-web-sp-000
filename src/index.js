8d5fdebdee0de4deee6e6cc80ee59b7fb48b770d
Paul Rosero <paul_rosero@yahoo.com>
2020-02-11 19:41:36 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
 console.log('%c HI', 'color: firebrick')
 
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-
 function fetchImg() {
-    return fetch("https://dog.ceo/api/breeds/image/random/4")
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
     .then(resp => resp.json())
-    .then(json => console.log(fetchImg(json))   
+    .then(results => {results.message.forEach(img => renderImg(img))})
 }
 
-function renderImg(json){
-    document.
+function renderImg(img){
+    const image = document.getElementById("dog-image-container")
+    let tag = document.createElement('img')
+    tag.src = img
+    image.appendChild(tag)
 }
 
 document.addEventListener('DOMContentLoaded', function() {
     fetchImg()
-  })
+})
+
+function fetchBreed() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(result => {
+        for (const breed in result.message) {
+            console.log(breed)
+            renderBreed(breed)
+        }
+    })
+}
+
+function renderBreed(breed) {
+    const breeds = document.getElementById("dog-breeds")
+    let tags = document.createElement('li')
+    tags.innerHTML = breed
+    breed.appendChild(tags)
+
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchBreed()
+})
