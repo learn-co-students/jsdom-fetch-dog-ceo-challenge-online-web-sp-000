eab7619d4135ed1430c5b9ebe8daca3d1c7f3ba1
rich0024 <yance0022@gmail.com>
2020-03-05 19:01:49 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchImages()
+    fetchBreeds()
+  }) 
+
+function fetchImages(){
+  json = fetch(imgUrl)
+  .then(response => response.json())
+  .then(json => renderImages(json))
+}
+
+function fetchBreeds(){
+  json = fetch(breedUrl)
+  .then(response => response.json())
+  .then(json => renderBreeds(json))
+}
+
