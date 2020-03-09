33153f33dd77cee23e0fcc79e685c97f9c245698
Amy Westcott <amy.westcott@yahoo.com>
2020-03-09 04:41:18 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+function fetchImages() {
+   const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+   fetch(imgUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+         imgActions(json)
+      })
+}
+
+function fetchBreeds() {
+   const breedUrl = "https://dog.ceo/api/breeds/list/all"
+
+   fetch(breedUrl)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(json) {
+         breedActions(json)
+      })
+}
