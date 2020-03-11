02bc5547d12b60ca8af843c95550e6619c888af7
tomciv9000 <eugene.white@gmail.com>
2020-03-10 22:14:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,72 +1,23 @@
-let breeds = [];
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let container = document.querySelector('#dog-image-container');
 
-document.addEventListener('DOMContentLoaded', function () {
-    loadImages();
-    loadBreedOptions();
-  });
-
-function loadImages() {
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-    fetch(imgUrl)
-      .then(resp => resp.json())
-      .then(results => {
-        results.message.forEach(image => addImage(image))
-      });
-  }
+function grabImages(){
+  fetch(imgUrl)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+    json.message.forEach(imageUrl => {
+      addImage(imageUrl)
+    });
+  })
+}
 
 function addImage(dogPicUrl) {
-  let container = document.querySelector('#dog-image-container');
+
   let newImageElement = document.createElement('img');
   newImageElement.src = dogPicUrl;
   container.appendChild(newImageElement);
 }
 
-function loadBreedOptions() {
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-    fetch(breedUrl)
-      .then(res => res.json())
-      .then(results => {
-  
-        breeds = Object.keys(results.message);
-        updateBreedList(breeds);
-        addBreedSelectListener();
-      });
-  }
-  
-  function updateBreedList(breeds) {
-    let ul = document.querySelector('#dog-breeds');
-    removeChildren(ul);
-    breeds.forEach(breed => addBreed(breed));
-  }
-  
-  function removeChildren(element) {
-    let child = element.lastElementChild;
-    while (child) {
-      element.removeChild(child);
-      child = element.lastElementChild;
-    }
-  }
-  
-  function selectBreedsStartingWith(letter) {
-    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
-  }
-  
-  function addBreedSelectListener() {
-    let breedDropdown = document.querySelector('#breed-dropdown');
-    breedDropdown.addEventListener('change', function (event) {
-      selectBreedsStartingWith(event.target.value);
-    });
-  }
-  
-  function addBreed(breed) {
-    let ul = document.querySelector('#dog-breeds');
-    let li = document.createElement('li');
-    li.innerText = breed;
-    li.style.cursor = 'pointer';
-    ul.appendChild(li);
-    li.addEventListener('click', updateColor);
-  }
-  
-  function updateColor(event) {
-    event.target.style.color = 'palevioletred';
-  }
+grabImages()
