bbec2d50becc8a60f7f96a6bf582cf337b110952
github username <github email address>
2020-03-26 12:27:41 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function(){
+    fetchImages()
+    fetchBreeds()
+})
+
+
+function fetchImages() {
+    const imageUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imageUrl)
+        .then(resp => resp.json())
+        .then(json => {json.message.forEach(image => addImage(image))})
+}
+
+function addImage(dogImage) {
+    let imageContainer = document.getElementById("dog-image-container")
+    let newImageElement = document.createElement("img")
+
+    newImageElement.src = dogImage
+    imageContainer.appendChild(newImageElement)
+}
+
+function fetchBreeds() {
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
+    fetch(breedUrl)
+        .then(resp => resp.json())
+        .then(json => { 
+            breeds = Object.keys(json.message)
+            .forEach(breed => addBreed(breed))
+        })
+}
+
+function addBreed(breeds) {
+    let breedList = document.getElementById("dog-breeds")
+    let breed = document.createElement("li")
+
+    breed.innerHTML = breeds
+    breedList.appendChild(li)
+}
