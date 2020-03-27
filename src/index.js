d28a4bba44c7b20c95277ad63b93c03ad8c60833
github username <github email address>
2020-03-27 09:46:25 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 console.log('%c HI', 'color: firebrick')
 
+let breeds = []
+
 document.addEventListener('DOMContentLoaded', function(){
     fetchImages()
     fetchBreeds()
 })
-
 
 function fetchImages() {
     const imageUrl = "https://dog.ceo/api/breeds/image/random/4"
@@ -25,16 +26,56 @@ function fetchBreeds() {
     const breedUrl = "https://dog.ceo/api/breeds/list/all"
     fetch(breedUrl)
         .then(resp => resp.json())
-        .then(json => { 
-            breeds = Object.keys(json.message)
-            .forEach(breed => addBreed(breed))
-        })
+        .then(results => { 
+            breeds = Object.keys(results.message);
+            updateBreedList(breeds);
+            addBreedSelectListener();
+        });
 }
 
-function addBreed(breeds) {
-    let breedList = document.getElementById("dog-breeds")
-    let breed = document.createElement("li")
+function updateBreedList(breeds) {
+    let ul = document.getElementById("dog-breeds");
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+}
 
-    breed.innerHTML = breeds
-    breedList.appendChild(li)
+function removeChildren (element) {
+    let child = element.lastElementChild;
+    while (child) {
+        element.removeChild(child);
+        child = element.lastElementChild;
+    }
+}
+
+function addBreed(breed) {
+    let ul = document.getElementById("dog-breeds")
+    let li = document.createElement("li")
+
+    li.innerHTML = breed
+    li.style.cursor = 'pointer'
+    ul.appendChild(li)
+    li.addEventListener('click', changeColor)
+}
+
+function changeColor(event) {
+    event.target.style.color = "red";
+}
+
+function addBreedSelectListener() {
+    let breedDropdown = document.getElementById("breed-dropdown")
+    breedDropdown.addEventListener("change", function(event) {
+        selectBreedsStartingWith(event.target.value)
+    })
+}
+
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)))
+}
+
+function addValuesToDropdown() {
+    let list = document.getElementById("breed-dropdown")
+    for (i = 0; )
+    let option = document.createElement("option")
+
+
 }
