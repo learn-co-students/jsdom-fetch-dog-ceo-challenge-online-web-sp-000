0ec41551b20e7f74b1fb38dc5075313c44abc75e
Azraf Chowdhury <achowdhur@ltu.edu>
2020-04-27 05:17:40 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,73 @@
-console.log('%c HI', 'color: firebrick')
+let breeds = [];
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+  loadImages();
+  loadBreedOptions();
+});
 
-  let dogImageContainer = document.getElementById("dog-image-container");
-  let dogBreeds = document.getElementById("dog-breeds");
-  let dogBreedDropdown = document.getElementById("breed-dropdown");
+function loadImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
 
-  return fetch("https://dog.ceo/api/breeds/image/random/4");
-    .then(resp => resp.json());
-    .then(json => renderImages(json.message));
+function addImage(dogPicUrl) {
+  let container = document.querySelector('#dog-image-container');
+  let newImageEl = document.createElement('img');
+  newImageEl.src = dogPicUrl;
+  container.appendChild(newImageEl);
+}
 
-  return fetch("https://dog.ceo/api/breeds/list/all");
-    .then(resp => resp.json());
-    .then(json => allDogBreeds(json.message));
+function loadBreedOptions() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(res => res.json())
+    .then(results => {
 
-    function dogImage(image) {
-      return `<img style="display:flex; width: 25%" class="dog-image" src=${image}>`
-    }
+      breeds = Object.keys(results.message);
+      updateBreedList(breeds);
+      addBreedSelectListener();
+    });
+}
 
-    function renderImages(images) {
-      dogImageContainer.innerHTML += images.map(dogImage).join("")
-    }
+function updateBreedList(breeds) {
+  let ul = document.querySelector('#dog-breeds');
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
 
-})
+function removeChildren(element) {
+  let child = element.lastElementChild;
+  while (child) {
+    element.removeChild(child);
+    child = element.lastElementChild;
+  }
+}
 
+function selectBreedsStartingWith(letter) {
+  updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+function addBreedSelectListener() {
+  let breedDropdown = document.querySelector('#breed-dropdown');
+  breedDropdown.addEventListener('change', function (event) {
+    selectBreedsStartingWith(event.target.value);
+  });
+}
+
+function addBreed(breed) {
+  let ul = document.querySelector('#dog-breeds');
+  let li = document.createElement('li');
+  li.innerText = breed;
+  li.style.cursor = 'pointer';
+  ul.appendChild(li);
+  li.addEventListener('click', updateColor);
+}
+
+function updateColor(event) {
+  event.target.style.color = 'palevioletred';
+}
 /* challenge 1 */
