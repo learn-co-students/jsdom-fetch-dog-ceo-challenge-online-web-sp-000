84dce6ed8c1c4a44aa7b00e751e93d961a609d53
github username <github email address>
2020-05-01 14:31:39 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,29 @@
 console.log('%c HI', 'color: firebrick');
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-const imageContainer = document.getElementById("dog-image-container");
+const imageContainer = document.getElementById('dog-image-container');
 
 
 function fetchDogs() {
   return fetch(imgUrl)
     .then(resp => resp.json())
-    .then(resp => renderDogs(resp)); 
+    .then(resp => renderDogs(resp.message)); 
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('load', function() {
   fetchDogs();
 });
 
 // ["https://images.dog.ceo/breeds/spaniel-welsh/n02102177_1028.jpg", "https://images.dog.ceo/breeds/akita/Akita_Dog.jpg", "https://images.dog.ceo/breeds/groenendael/n02105056_4600.jpg", "https://images.dog.ceo/breeds/terrier-russell/jack2.jpg"]
 
-
 function renderDogs(imageSourceArray) {
-  for (const image of imageSourceArray) {
-    let newImageEl = document.createElement('img');
+  debugger 
+
+  let newImageEl = document.createElement('img');
+  for (const image of imageSourceArray) { 
     newImageEl.src = image;
     imageContainer.appendChild(newImageEl);
   }
 
 }
 
-
-  // const main = document.querySelector('main')
-  // json.forEach(book => {
-  //   const h2 = document.createElement('h2')
-  //   h2.innerHTML = `<h2>${book.name}</h2>`
-  //   main.appendChild(h2)
-  // })
 
