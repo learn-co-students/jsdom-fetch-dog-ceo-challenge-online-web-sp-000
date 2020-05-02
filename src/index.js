4e41b58049424cda67ce481fa5e3751d792f3b9e
github username <github email address>
2020-05-02 17:03:43 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 const breedUrl = 'https://dog.ceo/api/breeds/list/all';
 
 
+
 document.addEventListener('DOMContentLoaded', function() {
   const breedList = document.getElementById('dog-breeds');
   fetchDogs();
@@ -11,30 +12,52 @@ document.addEventListener('DOMContentLoaded', function() {
 
 function addEventListenerToLetterSelect(){
   const letterSelectEl = document.getElementById("breed-dropdown");
-  letterSelectEl.addEventListener('change', filterBreeds);
+  //! below will have to not just load breeds
+  letterSelectEl.addEventListener('change', loadBreeds);
 };
 
-function filterBreeds () {
+function loadBreeds (breeds) {
   // get all breeds object
-  let breedsObject = fetchBreeds();
+  //let breedsObject = fetchBreeds();
+  //debugger
   // set variable for value of dropdown id (letter)
   const dropdownLetter = document.getElementById("breed-dropdown").value;
   console.log(dropdownLetter);
   // remove elements that are not filtered
   let parent = document.getElementById('dog-breeds');
   removeChildElements(parent);
-  //! not removing from browser, but removes all children in debugger --- whyyyyy?
+
+  for (const key in breeds) {
+    let childLi = document.createElement('li');
+    childLi = document.createElement('li');
+    debugger
+    childLi.value = key;
+    //! Why in debugger is key the breed name, but it cannot be be assigned to childLi. 
+    //! Once it is appended, the value is "0"
+    parent.appendChild(childLi);
+  }
   // filter all breeds object
-  filteredObj = function filterByLetter(breedsObject) {
-    for (const key in breedsObject)
-      if ((key.startsWith(dropdownLetter)) == false) {
-        delete result.key
-      };
-    return breedsObject;
-  };
-  renderBreeds(filteredObj);
-  console.log('In filterBreeds function');
+  // let filteredObj = function filterByLetter(breeds) {
+  //   for (const key in breeds)
+  //   debugger
+  //     if ((key.startsWith(dropdownLetter)) == false) {
+  //       delete breeds.key
+  //     };
+  //   return breeds;
+  // };
+  //renderBreeds(filteredObj(breeds));
+
+  console.log(breeds);
+  return breeds;
 };
+
+// function filterBreeds(loadBreeds(event)) {
+  // use event target value
+  // grab letter value
+
+  //update list
+//   console.log(breeds);
+// }
 
 function removeChildElements(parent) {
   while (parent.hasChildNodes()) {  
@@ -61,7 +84,11 @@ function fetchDogs() {
 function fetchBreeds() {
   return fetch(breedUrl)
     .then(resp => resp.json())
-    .then(resp => renderBreeds(resp.message)); 
+    .then(resp => { 
+      renderBreeds(resp.message);
+      loadBreeds(resp.message);
+    });  
+    
 }
 
 function renderDogs(imageSourceArray) {
