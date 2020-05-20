e91ef1f5eabcc3dd6bafb3282977da77cf7911f9
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-19 19:10:47 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,59 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('load', (event) => {
+    fetchImages()
+  })
+
+  function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => {
+        json.message.forEach(image => addImg(image))
+    });
+  }
+
+  function loadBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+      .then(res => res.json())
+      .then(results => {
+        breeds = Object.keys(results.message);
+        updateBreeds(breeds);
+        addBreedSelectListener();
+      });
+  }
+
+  function addBreedSelectListener() {
+    let breedDropdown = document.querySelector('#breed-dropdown');
+    breedDropdown.addEventListener('change', function (event) {
+      selectBreedsStartingWith(event.target.value);
+    });
+  }
+
+  function updateBreeds(breeds) {
+    let ul = document.querySelector('#dog-breeds');
+    removeChildren(ul);
+    breeds.forEach(breed => addBreed(breed));
+  }
+
+  function addBreed(breed) {
+    let ul = document.querySelector('#dog-breeds');
+    let li = document.createElement('li');
+    li.innerText = breed;
+    li.style.cursor = 'pointer';
+    ul.appendChild(li);
+    li.addEventListener('click', updateColor);
+  }
+
+  function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+  }
+
+  function updateColor(event) {
+    event.target.style.color = 'red';
+  }
