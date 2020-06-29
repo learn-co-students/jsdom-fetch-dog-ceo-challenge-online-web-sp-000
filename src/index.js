6d7b2911458027a15abff8bb3c9ed38c595415d2
Christopher Dent <christopherdent01@gmail.com>
2020-06-28 20:24:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,56 +1,157 @@
-//on page load
-//fetch the images using the url above ⬆️
-//parse the response as JSON
-//add image elements to the DOM for each🤔 image in the array
-const breedUrl = 'https://dog.ceo/api/breeds/list/all'
-const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+console.log('%c HI', 'color: firebrick')
 
-document.addEventListener('DOMContentLoaded', function () {
-    fetchImages();
-    fetchBreedList()
+let images={};
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchDogs()
+  fetchBreeds()
+  clickBreeds()
+  filterBreeds()
+})
+
+
+function fetchDogs() {
+  fetch('https://dog.ceo/api/breeds/image/random/4')
+  .then(response => response.json())
+  .then(data => {
+    let arr = data.message
+    arr.forEach(element => addImage(element));
   });
+};
 
-function fetchImages() {
-  fetch(imgUrl)
-  .then(resp => resp.json())
-  .then(json => json.message.forEach(image => addImages(image)));
-}
-
-function addImages(image) {
-    const container = document.getElementById('dog-image-container')
-    const newImage = document.createElement('img')
-    newImage.src = image
-    container.appendChild(newImage)
-}
-function fetchBreedList() {
-  fetch(breedUrl)
-  .then(resp => resp.json())
-  .then(json => {
-    breeds = Object.keys(json.message)
-    addBreeds(breeds)
-  })
-}
-function addBreeds(breeds) {
-  const ulBreeds = document.getElementById('dog-breeds')
-  breeds.forEach(breed => {
-      const newBreed = document.createElement('li')
-      newBreed.id = "onClick"
-      newBreed.innerText = breed
-      ulBreeds.appendChild(newBreed)
-      newBreed.addEventListener("click", changeColor)
-  })
-}
-function changeColor(newBreed) {
-newBreed.target.style.color = 'magenta'
-}
-
-function selectBreedsStartingWith(letter) {
-    addBreeds(breeds.filter(breed => breed.startsWith(letter)));
+  function addImage(dogPicUrl) {
+    let img = document.createElement("img");
+    let div = document.getElementById('dog-image-container')
+    img.src = dogPicUrl
+    div.appendChild(img)
   }
-function addBreedSelectListener() {
-const breedDropdown = document.querySelector('input[name="select-breed"]');
-breedDropdown.addEventListener('change', function(event) {
-  selectBreedsStartingWith(event.target.value);
-});
 
+function fetchBreeds() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data.message
+    for (const key in obj) {
+      addBreeds(key)
+    };
+  });
+};
+
+function addBreeds(breed) {
+  const ul = document.getElementById('dog-breeds')
+  const li = document.createElement('li');
+  li.innerText = breed
+  ul.appendChild(li)
+  clickBreeds()
 }
+
+function clickBreeds() {
+  let lis= document.querySelectorAll('li')
+  lis.forEach((element) => {
+    element.addEventListener('click', function(event) {
+      element.style="color:firebrick";  //maybe add code to make it change back upon another click?
+    })
+  })
+}
+
+//below I just need to replace the console.logs with methods that will only show the dogs of each letter.
+function filterBreeds() {
+
+  const drop = document.getElementById('breed-dropdown')
+  drop.addEventListener('click', function(event) {
+    if (drop.value === "a") {
+     console.log("Hi There A")
+     fetchABreeds()
+    }
+     if (drop.value === "b") {
+     console.log("Hi There B")
+     fetchBBreeds()
+    }
+   if (drop.value === "c") {
+     console.log("Hi There C")
+     fetchCBreeds()
+    }
+   if (drop.value === "d") {
+     console.log("Hi There D")
+     fetchDBreeds()
+    }
+  })
+}
+
+/// maybe like this ?
+
+function fetchABreeds() {
+  let letters = []
+  const ul = document.getElementById('dog-breeds')
+  ul.innerHTML = "" //clears list
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data.message
+    for (const key in obj) {
+      if (key.startsWith("a")){
+        letters.push(key)
+      };
+      for (const element of letters) {
+      addBreeds(element)
+    }
+    };
+  });
+};
+
+function fetchBBreeds() {
+  let letters = []
+  const ul = document.getElementById('dog-breeds')
+  ul.innerHTML = "" //clears list
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data.message
+    for (const key in obj) {
+      if (key.startsWith("b")){
+        letters.push(key)
+      };
+    for (const element of letters) {
+      addBreeds(element)
+    }
+    };
+  });
+};
+
+function fetchCBreeds() {
+  let letters = []
+  const ul = document.getElementById('dog-breeds')
+  ul.innerHTML = "" //clears list
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data.message
+    for (const key in obj) {
+      if (key.startsWith("c")){
+        letters.push(key)
+      };
+    for (const element of letters) {
+      addBreeds(element)
+    }
+    };
+  });
+};
+
+function fetchDBreeds() {
+  let letters = []
+  const ul = document.getElementById('dog-breeds')
+  ul.innerHTML = "" //clears list
+  fetch('https://dog.ceo/api/breeds/list/all')
+  .then(response => response.json())
+  .then(data => {
+    let obj = data.message
+    for (const key in obj) {
+      if (key.startsWith("d")){
+        letters.push(key)
+      };
+    for (const element of letters) {
+      addBreeds(element)
+    }
+    };
+  });
+};
