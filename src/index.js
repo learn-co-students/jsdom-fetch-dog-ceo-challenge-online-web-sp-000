27d3c6c20dd839f1b3b8f4c614439dfe0b549974
Anthony Tilelli <Anthony@tilelli.me>
2020-07-24 23:42:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+function getImages(Url) {
+  fetch(Url)
+    .then(responce => responce.json())
+    .then(data => data.message.forEach(item => appendImage(item)));
+}
+function appendImage(picSrc) {
+    const newImage = document.createElement('img');
+    newImage.src = picSrc;
+    document.getElementById('dog-image-container').appendChild(newImage);
+}
+
+function getBreeds(url) {
+  fetch(url)
+    .then(responce => responce.json())
+    .then(data => Object.keys(data.message).forEach(item => appendList(item)));
+}
+function appendList(item) {
+    const newli = document.createElement('li');
+    newli.innerText = item;
+    document.getElementById('dog-breeds').appendChild(newli);
+}
+
+function start(){ 
+    getImages(imgUrl);
+    getBreeds(breedUrl);
+
+}
+
+document.addEventListener('DOMContentLoaded', start());
