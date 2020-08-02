dfc2cae84307940dcb99bbd9e56bdf5aac1bad32
batsaikhan84 <u.batsaikhan@gmail.com>
2020-08-02 02:02:16 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -3,51 +3,46 @@ const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
 const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
 document.addEventListener('DOMContentLoaded', function() {
+
+    function valueSelector() {
+        let selector = document.getElementById("breed-dropdown").value
+        console.log(selector)
+    }
+
+    function fetchImage() {
+        return fetch(imgUrl)
+        .then(response => response.json())
+        .then(response => renderImages(response))
+    }
+    function renderImages(response) {
+        let image_container = document.getElementById("dog-image-container")
+        response.message.forEach(message => {
+            let image = document.createElement('img')
+            image.src = message
+            image_container.appendChild(image)
+        })
+    }
+    function fetchBreeds() {
+        return fetch(breedUrl)
+        .then(response => response.json())
+        .then(response => renderBreeds(response))
+    }
+    function renderBreeds(response) {
+        const myArray = Object.keys(response.message)
+        const ul = document.getElementById("dog-breeds")
+        for (const element of myArray) {
+            let li = document.createElement('li')
+            li.addEventListener("click", () => li.style.color = "red")
+            // let selector = document.getElementById("breed-dropdown").value
+            if (valueSelector() == element[0]) {
+                li.innerText = element
+                ul.appendChild(li)
+            }
+        }
+    }
+
     fetchImage()
     fetchBreeds()
-  })
-
-function fetchImage() {
-    return fetch(imgUrl)
-    .then(response => response.json())
-    .then(response => renderImages(response))
-}
-function renderImages(response) {
-    let image_container = document.getElementById("dog-image-container")
-    response.message.forEach(message => {
-        let image = document.createElement('img')
-        image.src = message
-        image_container.appendChild(image)
-    })
-}
-function fetchBreeds() {
-    return fetch(breedUrl)
-    .then(response => response.json())
-    .then(response => renderBreeds(response))
-}
-function renderBreeds(response) {
-    const myArray = Object.keys(response.message)
-    const ul = document.getElementById("dog-breeds")
-    for (const element of myArray) {
-        let li = document.createElement('li')
-        li.addEventListener("click", () => li.style.color = "red")
-        li.innerText = element
-        ul.appendChild(li)
-
-    }
-}
-function getOptionValue() {
-    let selected = document.getElementById("breed-dropdown");
-    let valueText = selected.options[selected.selectedIndex].text
-    console.log(valueText);
-}
-
-
-
-
-
-
-
-
+})
 
 
