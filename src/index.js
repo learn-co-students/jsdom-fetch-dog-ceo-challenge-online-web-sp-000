b998c343f89f3d86652d5ea7ffbbab7abd8c5033
mbade1 <mbade314@gmail.com>
2020-08-11 15:19:34 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+function loadImages() {
+    return fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(picUrl) {
+    const container = document.getElementById("dog-image-container");
+    const newImage = document.createElement("img");
+    newImage.src = picUrl;
+    container.appendChild(newImage);
+}
+
+function loadBreeds() {
+    return fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(results => {
+        const breeds = Object.keys(results.message);
+        addBreeds(breeds);
+    })
+}
+
+function addBreeds(breeds) {
+    const ul = document.getElementById("dog-breeds");
+    breeds.forEach(breed => {
+        const li = document.createElement("li");
+        li.innerText = breed;
+        ul.appendChild(li);
+        li.addEventListener('click', function(e) {
+            e.target.style.color = "blue";
+        });
+    });
+}
