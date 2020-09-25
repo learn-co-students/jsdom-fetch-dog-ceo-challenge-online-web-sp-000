d306c9a3f0d72e4e13740c38da9a106b3bd180b2
carrollm2 <carrollm2@gmail.com>
2020-09-24 23:19:29 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 console.log('%c HI', 'color: firebrick')
+
+
+function fetchImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+    fetch(imgUrl)
+      .then(resp => resp.json())
+      .then(json => {
+        json.message.forEach(image => renderImages(image))})        
+  }
+  
+  function renderImages(image) {
+    const main = document.getElementById('dog-image-container')
+    const newImage = document.createElement('img')
+    newImage.src = image;
+    main.appendChild(newImage); 
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchImages()
+  })
