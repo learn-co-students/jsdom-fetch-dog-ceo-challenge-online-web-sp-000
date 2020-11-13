40e143191d2a2dba3ad7f017ffa188e598c6246f
Snowing222 <snowing222@gmail.com>
2020-11-13 17:10:53 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('%c HI', 'color: firebrick')
+window.addEventListener('DOMContentLoaded', () => {
+    fetchImage()
+    fetchBreed()  
+});
+
+function fetchImage(){ 
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(resp => resp.json())
+    .then(json => showImage(json))
+}
+
+function showImage(json){
+    const divImage = document.getElementById("dog-image-container")
+    json.message.forEach(image => {
+    const img = document.createElement('img')
+    img.src = image
+    divImage.appendChild(img)
+    })     
+}
+
+function fetchBreed(){
+    fetch('https://dog.ceo/api/breeds/list/all')
+    .then(resp => resp.json())
+    .then(json => addBreed(json))
+}
+
+function addBreed(json){
+    const ul = document.getElementById("dog-breeds")
+    debugger
+
+
+}
