ba68cdd338fe83387f3208c41db89ea77f116089
Snowing222 <snowing222@gmail.com>
2020-11-14 15:04:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 window.addEventListener('DOMContentLoaded', () => {
     fetchImage()
     fetchBreed()
-    const lis= document.getElementsByTagName("li")
     
-    lis.forEach(li=> {
-        li.addEventListener("click",()=>{
-            li.style.color = "red"
-        })  
-    })
 })
+
 
 function fetchImage(){ 
     fetch("https://dog.ceo/api/breeds/image/random/4")
@@ -32,23 +27,50 @@ function fetchBreed(){
 }
 
 function addBreed(json){
-    const ul = document.getElementById("dog-breeds")
-
     for (const [key, value] of Object.entries(json.message)) {
-   
-        const li = document.createElement("li")
-        li.innerText = key
-        const subUl = document.createElement("ul")
-        for(const element of value){
-            const subLi = document.createElement("li")
-            subLi.innerText = element
-            subUl.appendChild(subLi)
-        }
-
-        li.appendChild(subUl)
-        ul.appendChild(li)
+        displayABreed(key, value)
       }
-     
 
+    const select = document.getElementById("breed-dropdown")
+    select.addEventListener("change",(event)=>{
+        const ul = document.getElementById("dog-breeds")
+        ul.innerText = ""
+        const letter = event.target.value
+        for (const [key, value] of Object.entries(json.message)){
+            if (key.charAt(0) === letter){
+                displayABreed(key,value)
+            }
+        }
+    })
+}
+
+function displayABreed(key, value){
+    const ul = document.getElementById("dog-breeds")
+    const li = document.createElement("li")
+    li.addEventListener("mousedown",()=>{
+        li.style.color = "red" 
+    })  
+
+    li.addEventListener("mouseup",()=>{
+        li.style.color ="black" 
+    })  
+
+    li.innerText = key
+    const subUl = document.createElement("ul")
+    for(const element of value){
+        const subLi = document.createElement("li")
+         subLi.addEventListener("mousedown",()=>{
+            subLi.style.color = "red" 
+         })  
+
+         subLi.addEventListener("mouseup",()=>{
+              subLi.style.color = "black" 
+         }) 
+          subLi.innerText = element
+          subUl.appendChild(subLi)
+     }
+
+      li.appendChild(subUl)
+      ul.appendChild(li)
 
 }
