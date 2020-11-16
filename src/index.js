569dd8da5f486a544dac9f5fbce4840a6ff5f9da
github username <jaycru06@yahoo.com>
2020-11-15 20:13:30 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log('%c HI', 'color: firebrick')
+console.log('%c HI', 'color: firebrick');
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchImage() {
+   fetch(imgUrl)
+   .then(response => response.json())
+   .then(json => renderImages(json));
+}
+
+function renderImages(json) {
+   const dogImgContainer = document.querySelector("#dog-image-container");
+   Object.keys(json).forEach(dogImg => {
+      const img = document.createElement("img");
+      img.src = dogImg.src;
+      img.alt = "Image of a dog";
+      dogImgContainer.appendChild(img);
+   })
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+   fetchImage();
+})
