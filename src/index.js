ef54eaf43bbc14e75b9b38974efa247347dbb9c3
chienleow <chienleow1@gmail.com>
2020-11-17 17:31:52 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 console.log('%c HI', 'color: firebrick')
 
 function fetchImages(){
-    return fetch("https://dog.ceo/api/breeds/image/random/4")
-    .then(function(response){
-        return response.json();
-    })
-    .then(function(json){
-        const images = json["message"]
-        addImages(images)
-    })
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response => response.json())
+    .then(images => {
+        images.message.forEach(image => appendImages(image))
+    });
 }
 
-function addImages(images){
-
+function appendImages(links){
+    const container = document.querySelector("#dog-image-container");
+    const dogImage = document.createElement("img");
+    dogImage.src = links;
+    container.appendChild(dogImage);
 }
 
 document.addEventListener('DOMContentLoaded', function(){
