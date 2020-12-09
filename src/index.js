f4a152b3adfed3fddb9dfadb6ab4aab6b531f16c
tbanish <tombanish@gmail.com>
2020-12-08 19:18:06 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+fetch(imgUrl)
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    const dogContainer = document.querySelector("div#dog-image-container")
+
+    for(const image of json.message) {
+      dogContainer.innerHTML += `<img src=${image}>`
+    }
+  });
+
+fetch(breedUrl)
+  .then(function(response){
+    return response.json();
+  })
+
+  .then(function(json){
+    const dogBreedList = document.querySelector("ul#dog-breeds")
+    const dropDown =document.getElementById("breed-dropdown")
+    const breeds = [];
+
+    for(const breed in json.message) {
+      dogBreedList.innerHTML += `<li>${breed}</li>`
+      breeds.push(breed)
+    }
+    dogBreedList.addEventListener("click", function(event){
+      event.target.style.color = "blue";
+    })
+    dropDown.addEventListener("change", function(event){
+      const letter = event.target.value;
+      const filteredList = breeds.filter((breed) => breed.startsWith(letter))
+      dogBreedList.innerHTML = ''
+
+      for(const filteredBreed of filteredList) {
+        dogBreedList.innerHTML += `<li>${filteredBreed}</li>`
+      }
+    })
+})
