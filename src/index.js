75172644f5982a71815bc96062d39c66be963142
Alexander M. Clamor <alexander.clamor@gmail.com>
2020-12-17 02:06:10 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const divDogs = document.getElementById("dog-image-container");
+
+function fetchImages() {
+    fetch(imgUrl)
+        .then(resp => resp.json())
+        .then(json => renderImages(json));
+}
+
+function renderImages(json) {
+
+    Array.prototype.forEach.call(json, src => {
+        const img = document.createElement('IMG');
+        img.setAttribute("src", src);
+        divDogs.appendChild(img);
+    })
+
+    // json.forEach(src => {
+    //     console.log(src);
+    //     const img = documnet.createElement('IMG');
+    //     img.setAttribute("src", src);
+    //     divDogs.appendChild(img);
+    // })
+}
+
+document.addEventListener("DOMContentLoaded", fetchImages);
