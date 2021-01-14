65254ebccb6b0b8af98be0f5c200787a67d5900f
Ronda Cowen <cowenhome5@gmail.com>
2021-01-13 20:59:46 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
-console.log('%c HI', 'color: firebrick')
+// console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all" 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    loadImages();
+});
+
+function loadImages() {
+    fetch(imgUrl)
+    .then(results=> results.json())
+    .then(results => {
+       results.message.forEach(image => newImage(image))
+});
+}
+function newImage() {
+    let container = document.getElementById('dog-image-container');
+    // console.log(container)
+    let dogPicA = document.createElement('img');
+    console.log(dogPicA)
+
+    
+}
