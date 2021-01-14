64394e95f04232f3763fb2d068fbf630101f208e
Ronda Cowen <cowenhome5@gmail.com>
2021-01-14 14:19:59 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -16,12 +16,14 @@ function loadImages() {
        results.message.forEach(image => newImage(image))
 });
 }
+
 function newImage(image) {
     let container = document.getElementById('dog-image-container');
     let dogPicA = document.createElement('img');
     dogPicA.src = image
     container.append(dogPicA)
 }
+
 function loadBreeds(){
     fetch(breedUrl)
     .then(results=> results.json())
@@ -29,16 +31,18 @@ function loadBreeds(){
         breeds = Object.keys(results.message);
         updateBreedList(breeds);
         selectListener();
-           
-});
+    });
+}
 
 function updateBreedList(breeds) {
     let ul = document.querySelector('#dog-breeds');
-    breeds.forEach(breed => addBreed(breed));
-    
+    ul.querySelectorAll('*').forEach(n => n.remove());
+    breeds.forEach(breed => addBreed(breed));    
    }
- }
+ 
+
 function addBreed(breed){
+    
     let ul = document.querySelector('#dog-breeds');
     let li = document.createElement('li');
     li.innerText = breed;
@@ -46,16 +50,21 @@ function addBreed(breed){
     li.addEventListener('click', () => {li.style.color='red'});
     // console.log(breed)
  }
+
+ function filterBreeds(letter){
+     
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter))
+    );
+}
+ 
 function selectListener() {
 let breedDropDown = document.getElementById('breed-dropdown');
- debugger
     breedDropDown.addEventListener('change', (event) => {
+    
         filterBreeds(event.target.value);
+        // console.log(event.target.value)
          });
 
 }
     
-function filterBreeds(letter){
-    updateBreedList(breeds.filter(breed => breed.startsWith(letter))
-    );
-}
+
