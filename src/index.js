4ea75d4fa2dc9a50f1f4518cbcc0cf3246a117fc
William Pannell <lpann5@gmail.com>
2021-01-22 20:04:50 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function loadImages() {
-  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
-  fetch(imgUrl)
+  fetch("https://dog.ceo/api/breeds/image/random/4")
     .then(res=> res.json())
     .then(results => {
       results.message.forEach(image => addImage(image))
