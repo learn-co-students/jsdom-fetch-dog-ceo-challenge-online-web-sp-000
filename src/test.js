780605529accfbe443bdb58a67b7fbc6ee4409c7
Mike Barberry <mbarberry@Mikes-MacBook-Pro.local>
2021-02-09 23:24:08 -0800

    Done.

---
+++ src/test.js
@@ -0,0 +1,12 @@
+const images = {
+    "message":
+    [
+        "https:\/\/images.dog.ceo\/breeds\/shihtzu\/n02086240_7195.jpg",
+        "https:\/\/images.dog.ceo\/breeds\/komondor\/n02105505_769.jpg",
+        "https:\/\/images.dog.ceo\/breeds\/african\/n02116738_9282.jpg",
+        "https:\/\/images.dog.ceo\/breeds\/kelpie\/n02105412_2915.jpg"
+    ],
+    "status":"success"
+}
+
+console.log(images['message'][0])
