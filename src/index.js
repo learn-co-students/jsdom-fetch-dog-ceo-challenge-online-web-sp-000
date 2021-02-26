74d032ea261a4b1a6317df3c4f7c98b7ed4ee56c
Chandra Lama <iamme24cl@gmail.com>
2021-02-26 13:29:42 -0800

    .

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', function () {
+  addImages();
+});
+
+function addImages() {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+  fetch(imgUrl)
+    .then(response => response.json())
+    .then(results => {
+      results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(url) {
+  let container = document.getElementById('dog-image-container');
+  let imgElem = document.createElement('img');
+  imgElem.src = url;
+  container.appendChild(imgElem);
+}
