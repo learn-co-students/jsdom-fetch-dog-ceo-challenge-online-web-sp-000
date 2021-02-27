6dbb1784006f424a3748dc28ac7d55fe67250a34
Chandra Lama <iamme24cl@gmail.com>
2021-02-26 20:18:56 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+let breeds = [];
 
 document.addEventListener('DOMContentLoaded', function () {
   addImages();
+  loadDogBreeds();
 });
 
 function addImages() {
@@ -17,4 +19,53 @@ function addImage(url) {
   let imgElem = document.createElement('img');
   imgElem.src = url;
   container.appendChild(imgElem);
+}
+
+function loadDogBreeds() {
+  const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+  fetch(breedUrl)
+    .then(response => response.json())
+    .then(results => {
+      breeds = Object.keys(results.message);
+      updateBreeds(breeds);
+      addBreedSelectionListener();
+    })
+}
+
+function updateBreeds(breeds) {
+  let ul = document.getElementById("dog-breeds")
+  removeChildren(ul);
+  breeds.forEach(breed => addBreed(breed));
+}
+
+function removeChildren(elem) {
+  let child = elem.lastElementChild;
+  while (child) {
+    elem.removeChild(child);
+    child = elem.lastElementChild;
+  }
+}
+
+function addBreed(breed) {
+  let ul = document.getElementById("dog-breeds");
+  let li = document.createElement("li");
+  li.innerText = breed;
+  ul.appendChild(li);
+  li.addEventListener("click", changeColor);
+}
+
+function changeColor(event) {
+  event.target.style.color = "red";
+}
+
+function filterBreedsBy(letter) {
+  updateBreeds(breeds.filter(breed => breed.startsWith(letter)));
+}
+
+
+function addBreedSelectionListener() {
+  let dropdown = document.querySelector("#breed-dropdown");
+  dropdown.addEventListener("change", function (event) {
+    filterBreedsBy(event.target.value);
+  });
 }
