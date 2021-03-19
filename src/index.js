ce71172c318d791b81fcb87253065aa7c99b981c
romanolivi <romano_livi@outlook.com>
2021-03-19 14:43:13 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,27 +1,52 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', () => {
+    let allBreeds = []
 
-    const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
-    const breedUrl = 'https://dog.ceo/api/breeds/list/all';
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    const breedUrl = "https://dog.ceo/api/breeds/list/all"
 
-    function fetchImages() {
-        fetch(imgUrl)
-            .then(resp => resp.json())
-            .then(json => {
-                dogImages(json.message)
-            })
-    }
-        
+    const dogImgContainer = document.getElementById('dog-image-container')
+    const dogBreedUl = document.getElementById('dog-breeds')
+    const breedDropdown = document.getElementById('breed-dropdown')
 
-    function dogImages(dogs) {
-        console.log(dogs)
-        dogs.forEach(dog => {
-            console.log(dog)
-            const dogContainer = document.querySelector('#dog-image-container')
-            const dogInContainer = document.createElement('img')
-            dogInContainer.src = dog
-            dogContainer.appendChild(dogInContainer)
-        });
-    }
+    dogBreedUl.addEventListener('click', function(e) {
+       e.target.style.color = 'red'
+    })
 
+    breedDropdown.addEventListener('change', (event) => {
+        const letter = event.target.value 
+        const filterBreeds = allBreeds.filter((breed) => breed.startsWith(letter))
 
-    
+        dogBreedUl.innerHTML = createDogList(filterBreeds)
+    })
+
+    fetch(imgUrl, { method: 'GET' })
+    .then( (response) => {
+        console.log(response)
+        if (response.ok) {
+            return response.json()
+        }
+    })
+    .then((dogImgData) => {
+        dogImgData.message.forEach(function(imgUrl) {
+            dogImgContainer.innerHTML += `<img src="${imgUrl}">`
+        })
+        const dogImgString = dogImgData.message.map((imgUrl) => {
+            return `<img src="${imgUrl}">`
+        })
+    })
+
+    fetch(breedUrl, { method: 'GET' })
+    .then((resp) => resp.json())
+    .then((breedData) => {
+        allBreeds = Object.kets(breedData.message)
+        console.log(allBreeds)
+        dogBreedUl.innerHTML = createDogList(allBreeds)
+    })
+})
+
+function createDogList(dogBreedArray) {
+    const dogListStringArray = dogBreedArray.map(function(breed) {
+        return `<li>${breed}</li>`
+    })
+    return dogListStringArray.join('')
+}
