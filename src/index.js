1989cbef4a1e1f79e6662e0b01a8aab94b55c823
TrueC-D <aletheacary@gmail.com>
2021-03-26 21:22:11 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -51,15 +51,11 @@ function renderDogImages(){
 
 function renderDogBreeds(){
 
+    
+
     const dogBreedUl = document.getElementById('dog-breeds')
 
     const urlToUse = 'https://dog.ceo/api/breeds/list/all'
-
-    function removeDogBreeds(){
-        // for(const dog of document.getElementsByTagName('li')){dog.remove()}
-        // document.getElementsByTagName('li').forEeach(dog => console.log(dog))
-
-    }
     
     // function addDogBreed(event) { 
     //     let dogBreed = document.createElement('li'); 
@@ -71,29 +67,36 @@ function renderDogBreeds(){
     // in an improved version i can add a function like this to make it DRY, but currently its not being called.  Will repeat to save time.
 
         const whatToReturn = function(json){
+            const breedDropdown = document.getElementById('breed-dropdown');
             for(const objectKey in json.message){
-            // addDogBreed(objectKey)
-            let dogBreed = document.createElement('li'); 
-            dogBreed.innerText = objectKey;
-            dogBreedUl.appendChild(dogBreed);
-            dogBreed.addEventListener('click', colorChange)
-        };
-        const breedDropdown = document.getElementById('breed-dropdown')
-        breedDropdown.onchange = function(){
-             console.log(breedDropdown.value);
-            //  removeDogBreeds();
-             const filteredByLetter = document.getElementsByTagName('li').filter(item => item.innerText.startsWith(breedDropdown.value));
-             for(const dog of filteredByLetter){
-                //  addDogBreed(dog); 
+                // addDogBreed(objectKey)
                 let dogBreed = document.createElement('li'); 
-                dogBreed.innerText = dog;
+                dogBreed.innerText = objectKey;
                 dogBreedUl.appendChild(dogBreed);
-                dogBreed.addEventListener('click', colorChange);
+                dogBreed.addEventListener('click', colorChange)
+            };
+
+            function removeDogBreeds(){
+                let curretList = Object.values(document.getElementsByTagName('li'))
+                curretList.map(dog => dog.remove())
+                // for(const dog of document.getElementsByTagName('li')){dog.remove()};
+                // document.getElementsByTagName('li').forEeach(dog => console.log(dog));
+            };
+            
+            const lis = document.getElementsByTagName('li');
+            const allDogs = Object.values(lis);
+            breedDropdown.onchange = function(){
+                removeDogBreeds();
+                const filteredByLetter = allDogs.filter(item => item.innerText.startsWith(breedDropdown.value));
+                for(const dog of filteredByLetter){
+                    //  addDogBreed(dog); 
+                    let dogBreed = document.createElement('li'); 
+                    dogBreed.innerText = dog.innerText;
+                    dogBreedUl.appendChild(dogBreed);
+                    dogBreed.addEventListener('click', colorChange);
+                }
             }
-            }
-        //I need to create an array for all Dogs and one for each drop down letter 
-        // It needs to populate all dogs at first, then delete remove all children from the Ul and add the array by letter
-}
+        }
     fetchDogs(urlToUse, whatToReturn)
 }
 
