3a6a75e125af4d3ee9bae1f1c706cfcd5273506c
kkirby16 <kkirby16@my.whitworth.edu>
2021-04-23 13:44:02 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 console.log('%c HI', 'color: firebrick')
 
+let breedNameArray
 
 document.addEventListener('DOMContentLoaded', function() { 
     fetchImages();
     fetchBreeds();
     breedSelectListener();
-    loadBreedOptions();
 });
 
 function fetchImages() { 
 fetch('https://dog.ceo/api/breeds/image/random/4')
   .then(resp => resp.json())
   .then(json => renderImages(json))
+
 }
 
   function renderImages(images) { 
@@ -27,8 +28,12 @@ fetch('https://dog.ceo/api/breeds/image/random/4')
   function fetchBreeds() { 
       fetch('https://dog.ceo/api/breeds/list/all')
       .then(resp => resp.json())
-      .then(json => renderBreeds(json))
+      .then(json => {  
+          renderBreeds(json)
+            breedNameArray = Object.keys(json.message)
+        })
   }
+
 
   function renderBreeds(breeds) { 
       let body = document.querySelector('body');
@@ -59,31 +64,38 @@ fetch('https://dog.ceo/api/breeds/image/random/4')
   function breedSelectListener() {
   let dropdownBox = document.getElementById("breed-dropdown");
   dropdownBox.addEventListener("change", (e) => { 
-      sortedBreeds(e.target.value);
+      updateBreedList(e.target.value);
       //put out sorted breed array elements to the dom in the ul that holds dog breeds. make this ul have the sorted breeds instead. 
    } )
 }
 
    function sortedBreeds(letter) { 
-    let breedList = document.querySelector("ul").children;
-    let breedArray = [...breedList];
-
-    let breedNameArray = breedArray.map(function(element) {
-    return element.innerText;
-    });
-    breedNameArray.filter((breed) => breed.startsWith(letter)); 
+    return breedNameArray.filter((breed) => breed.startsWith(letter)); 
    }
 
-   function loadBreedOptions() { 
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then(resp => resp.json())
-    .then(json => renderBreeds(json))
+   function updateBreedList(letter) { 
+  sortedBreeds(letter);
+  let body = document.querySelector('body');
+  let ul = document.getElementById('dog-breeds');
+  ul.innerHTML = "";
+  sortedBreeds(letter).forEach(element => {
+      let li = document.createElement("li")
+      li.innerHTML = element; 
+      ul.appendChild(li);
+  })};
 
-    for (const breed in breedSelectListener()) { 
-        let body = document.querySelector('body');
-        let ul = document.getElementById('dog-breeds');
-        let li = document.createElement('li');
-        li.innerText = `${breed}`;
-        ul.appendChild(li);
-   }
-};
+
+
+  //find document body 
+  //grab the ul by it's id. 
+  //ul.clear to clear that full list of breeds
+  //iterating through an array and then for each element creating an li 
+  //ul.appendChild(li)
+
+//     for (const breed in ()) { 
+//         let body = document.querySelector('body');
+//         let ul = document.getElementById('dog-breeds');
+//         let li = document.createElement('li');
+//         li.innerText = `${breed}`;
+//         ul.appendChild(li);
+//    }
