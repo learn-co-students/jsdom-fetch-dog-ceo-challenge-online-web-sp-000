d2e7fa15140a24afac5d2de99ab263e885cb083b
rmholsen <jaguar.kitty@gmail.com>
2021-05-10 15:54:06 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,31 +1,41 @@
 console.log('%c HI', 'color: firebrick');
 
-document.addEventListener('DOMContentLoaded', callbackfunc () {
+let breeds = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    //remember, that 'function' is an actual command, not an arbitrary name
     loadImg();
+    loadBreeds();
 });
 
 // sets up functions to run as soon as the DOM is loaded 
 
+// ###### IMAGE FUNCTIONS FOLLOW ######
+
 function loadImg() {
-    const imgURL = "https://dog.ceo/api/breeds/image/random/4"
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
     // URL to fetch
-    fetch(imgURL).then(res=> res.json()).then(
+    fetch(imgUrl)
+    .then(res=> res.json())
+    .then(results => {
     // Fetch the URL, turn the URL's contents into JSON, then do stuff to it
-        results => { results.message.forEach(image => addImg(image)) }
+         results.message.forEach(image => addImg(image))    
         // For each image load it into the DOM using the "addImg" function
-    );
+    });
 };
 
-function addImg(image) {
+function addImg(doggo) {
     let container = document.querySelector('#dog-image-container');
     // Remember the querySelector method needs the # in front of the class tag
     let newImg = document.createElement('img');
     // Remember to create the element before you stick something in it
-    newImg.src = image; 
+    newImg.src = doggo; 
     // <img src="image">
     container.appendChild(newImg);
     // shove the <img src="image"> into the tag with the right class
 };
+
+// ###### BREED LIST FUNCTIONS FOLLOW ######
 
 function loadBreeds() {
     const breedURL = "https://dog.ceo/api/breeds/list/all";
@@ -34,7 +44,8 @@ function loadBreeds() {
         results => {
             breeds = Object.keys(results.message);
             // assigns the results as keys in a new 'breeds' object
-
+            updateBreeds(breeds);
+            addBreedSelectListener();
         });
 }
 
@@ -49,6 +60,32 @@ function updateBreeds(breeds) {
     // iterate over teh array and add each one again
 }
 
+function addBreedSelectListener() {
+    let breedMenu = document.querySelector("#breed-dropdown");
+    // Grab the class id again
+    breedMenu.addEventListener('change', function (event) {
+        selectBreedLetter(event.target.value);
+    });
+    // on change (which is code speak for picking the menu item in the dropdown) do the thing Zhu Li
+    //In this case the thing is the function selectBreedLetter with an argument of whatever the target value is.
+}
+
+function selectBreedLetter(letter) {
+    updateBreeds(breeds.filter(breed => breed.startsWith(letter)));
+    // 
+}
+
+// function updateBreeds(breeds) {
+//     let ul = document.querySelector("#dog-breeds");
+//     // select the right element, remember the # for the class tag
+//     //ul.children.forEach(child => child.delete?)
+//     //no
+//     removeChildren(ul);
+//     // clear out the list
+//     breeds.forEach(breed => addBreed(breed));
+//     // iterate over teh array and add each one again
+// }
+
 function addBreed(breed) {
 // remember you have to do this the long way around. annoying, isn't it.
     let ul = document.querySelector('#dog-breeds');
@@ -59,7 +96,7 @@ function addBreed(breed) {
     li.addEventListener('click', changeColor);
 }
 
-function removeChild(el) {
+function removeChildren(el) {
     let child = el.lastElementChild;
     while (child) {
         el.removeChild(child);
@@ -69,7 +106,7 @@ function removeChild(el) {
 }
 
 function changeColor(click) {
-    click.target.style.color = 'lightblue';
+    click.target.style.color = 'hotpink';
 }
 
 
