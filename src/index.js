c34240996b6cfa1a655a81655ac17da1ea409a31
Technicallyaustin <Technicallyaustin@gmail.com>
2021-06-17 11:51:12 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+  fetch(imgUrl)
+  .then(response => response.json())
+  .then(result => {
+    dogPics(result.message)
+  })
+
+  function dogPics(dogs){
+    console.log(dogs)
+    const newUl = document.createElement('ul')
+    const dogContainer = document.querySelector('#dog-image-container')
+    dogContainer.appendChild(newUl)
+    dogs.forEach(dog => {
+      console.log(dog)
+      const picture = document.createElement('img')
+      picture.src = dog
+      dogContainer.appendChild(picture)
+    });
+  } 
